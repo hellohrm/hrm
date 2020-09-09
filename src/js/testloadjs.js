0a79daa70a1c9d781b8d71417e6d1b1d6f40022e
caicangcua <36462470+caicangcua@users.noreply.github.com>
2020-09-09 09:14:50 +0700

    v2

---
+++ src/js/testloadjs.js
@@ -0,0 +1,11 @@
+﻿function bind_click() {
+    document.getElementById('contact_zalo').addEventListener("click", function () {
+        alert('contact_zalo');
+    }, true);
+    document.getElementById('contact_tdn').addEventListener("click", function () {
+        alert('contact_tdn');
+    }, true);
+    document.getElementById('contact_email').addEventListener("click", function () {
+        alert('contact_email');
+    }, true);
+}
