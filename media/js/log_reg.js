259aa68d85a874289579642e256d1e0268f2c6cd
caicangcua <36462470+caicangcua@users.noreply.github.com>
2021-04-10 14:55:17 +0700

    Update log_reg.js

--- media/js/log_reg.js
+++ media/js/log_reg.js
@@ -267,9 +267,8 @@ var LoginModalController = {
         };
         //
         base.lockfrm('');
-        loginpw.remove();//.attr("type", "text");
         //
-        var that = $(_ist), dat = frm.serializeArray(); dat.push({ name: 'token', value: token.val() }, { name: 'session', value: hash[0] }, { name: "dogword", value: loginpw.val() }, { name: "doguser", value: email.val() });
+        var that = $(_ist), dat = frm.serializeArray(); dat.push({ name: 'token', value: token.val() }, { name: 'session', value: hash[0] }, { name: "dogword", value: loginpw.val() }, { name: "email", value: email.val().replace('@', '___xyz___') });
         base.hwData('/dangnhap?XDEBUG_SESSION_START=154A5348', dat, function (cb) {
             if (cb.rst == 'ok') {
                 var kq = JSON.parse(cb.dat);
@@ -286,7 +285,7 @@ var LoginModalController = {
                     });
                     base.lockfrm('none');
                 } else {
-                    //window.parent.postMessage({ 'msgtype': 'session', 'msgkind': hash[0], 'evtData': { evt: 'loginok', origin: window.location.origin, html: kq.html } }, hash[2]);
+                    window.parent.postMessage({ 'msgtype': 'session', 'msgkind': hash[0], 'evtData': { evt: 'loginok', origin: window.location.origin, html: kq.html } }, hash[2]);
                 };
             }
         });
