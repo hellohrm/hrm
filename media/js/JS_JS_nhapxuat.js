9eb4fb6c45dde107d36872f8ef1deaffec33eae9
caicangcua <36462470+caicangcua@users.noreply.github.com>
2023-03-30 13:04:11 +0700

    v1

--- media/js/JS_JS_nhapxuat.js
+++ media/js/JS_JS_nhapxuat.js
@@ -5,6 +5,8 @@
     function O(opts) { // constructor
 
         var _tabId, $mainO,
+            evtWK,
+            calcSession,
             elUI = {};
 
         this.init = function (frmEL, cb, tabId, clickArgs, tabProps) {
@@ -16,16 +18,78 @@
             elUI.lan = JSON.parse(JSON.parse(atob(init_dataDIV.text())).lan);
             init_dataDIV.remove();
 
+
+
+            //var iframeWin = frmEL.find("#da-iframe")[0].contentWindow;
+
+            //debugger;
+            //frmEL.find("#da-iframe").load(function () {
+            //    debugger;
+            //    $(this).contents().find("#m_excelEmbedRenderer_m_ewaEmbedViewerBar").css("height", "550px");
+            //});
+
             frmEL.on('shown.bs.tab', function (e) {
-                debugger;
+                //debugger;
+                //iframeWin.postMessage('myMessage.value', "*");
+
             }).find('[data-lang]').each(function (idx, el) {
                 var key = el.getAttribute('data-lang');
                 $(el).html(elUI.lan[key]);
             });
 
+
+
+            calcSession = new Date().getTime();
+            evtWK = document.createElement('iframe');
+            evtWK.setAttribute("style", "height:1px;width:1px;border:none;opacity:0");
+            //
+            evtWK.onload = function () {
+                //
+                debugger;
+                var iDoc = evtWK.contentWindow.document
+                    , attcalc = iDoc.createElement('script');
+
+                apisvr.a$.sessions[calcSession] = {
+                    id: calcSession,
+                    fn: function (a, b, c) {
+                        if (a == 'job') {
+                            if (b.evtData.messageType == 0) {//calc ready !
+                                debugger;
+                                //setTimeout(function () {
+                                //    iniS3DAT(function (existDAT) {
+                                //        d0n();//message thoi ma
+                                //        _$$();
+                                //    });
+                                //});//release handle onmessage
+                            };
+                        };
+                    }
+                };
+                //
+                //
+                attcalc.setAttribute('seson', btoa(calcSession + '|' + w0w.location.origin + '|' + (!w0w.Worker || true)));
+                //'/media/js/attcalc.js'
+                attcalc.setAttribute('src', srcpf$ + "\x2F\x6D\x65\x64\x69\x61\x2F\x6A\x73\x2Fim_nhanvien.js" + src$id);//'https://hrm.dnd.vn'
+                iDoc.head.appendChild(attcalc);
+                //
+                //
+            };
+            //
+          
+
+
+    
+
+
+
             admin$.DEV(function () {
                 //
-  
+                debugger;
+                //var c=frmEL.find("#da-iframe").contents();
+                //var d=c.find("#m_excelEmbedRenderer_m_ewaEmbedViewerBar");
+                //
+                // div tag in which iframe will be added should have id attribute with value myDIV
+                frmEL.find('.im_exfile').prepend(evtWK);//append vao step 3 main div
                 //
                 srclocked(false);
                 //
---
+++ media/js/im_nhanvien.js
@@ -0,0 +1,49 @@
+﻿(function () {
+
+    debugger;
+
+    var session = '0'
+    , orgMsg = '*'
+    //, Dy = parent.dayjs
+    , srpt = document.getElementsByTagName("script");
+
+
+
+
+    for (var i = 0; i < srpt.length; i++) {
+        var dat = srpt[i].getAttribute('seson');
+        if (dat) {
+            dat = atob(dat).split('|');
+            session = dat[0];
+            orgMsg = dat[1];
+            //
+        }
+    };
+    //**************************** post load *********************************************//
+    window.parent.postMessage({ 'msgtype': 'session', 'msgkind': session, 'evtData': { messageType: 0 } }, orgMsg);
+
+
+
+    function hwndMsg(evt) {
+        //debugger;
+        var message;
+        if (evt.origin !== "https://robertnyman.com") {
+            message = "You are not worthy";
+        }
+        else {
+            message = "I got " + evt.data + " from " + evt.origin;
+        }
+        document.getElementById("received-message").innerHTML = message + new Date().getTime();
+    };
+
+    if (window.addEventListener) {
+        // For standards-compliant web browsers
+        window.addEventListener("message", hwndMsg, false);
+    }
+    else {
+        window.attachEvent("onmessage", hwndMsg);
+    };
+
+
+
+})();
