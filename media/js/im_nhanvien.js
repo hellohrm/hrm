52a4a747c50cf0ace227e6a27ab0b3545356dab1
caicangcua <36462470+caicangcua@users.noreply.github.com>
2023-03-30 14:05:30 +0700

    Update im_nhanvien.js

--- media/js/im_nhanvien.js
+++ media/js/im_nhanvien.js
@@ -1,46 +1,7 @@
 ﻿(function () {
 
-
     debugger;
     //
-    var dog = document.querySelectorAll('body > div');
-    if (dog.length > 0) document.body.removeChild(dog[0]);
-    //
-    //
-    debugger;
-    //
-    var dat = atob(window.location.search.split('?seson=')[1]).split('|')
-        , session = dat[0]
-        , orgMsg = dat[1];
-    //
-    //
-    //
-    function hwndMsg(evt) {
-        //debugger;
-        var message;
-        if (evt.origin !== "https://robertnyman.com") {
-            message = "You are not worthy";
-        }
-        else {
-            message = "I got " + evt.data + " from " + evt.origin;
-        }
-        document.getElementById("received-message").innerHTML = message + new Date().getTime();
-    };
-
-    if (window.addEventListener) {
-        // For standards-compliant web browsers
-        window.addEventListener("message", hwndMsg, false);
-    }
-    else {
-        window.attachEvent("onmessage", hwndMsg);
-    };
-
-
-
-    //**************************** post load *********************************************//
-    window.parent.postMessage({ 'msgtype': 'session', 'msgkind': session, 'evtData': { messageType: 0 } }, orgMsg);
-
-
 
 
     //**************************** LOAD IF *********************************************//
@@ -51,7 +12,6 @@
     //iframe.src = "http://java2s.com";
     //document.body.appendChild(iframe);
     //iframe.onload = my_code;
-
 
 
 
