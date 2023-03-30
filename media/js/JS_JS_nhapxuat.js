d473f5257525974cdbce0fb1fa6341d5c2b34291
caicangcua <36462470+caicangcua@users.noreply.github.com>
2023-03-30 15:46:23 +0700

    Update JS_JS_nhapxuat.js

--- media/js/JS_JS_nhapxuat.js
+++ media/js/JS_JS_nhapxuat.js
@@ -1,177 +1,136 @@
-﻿(function () {
+﻿w0w.tabglobalJS['JS_JS_nhapxuat'] = (function () { // scoping
 
-    debugger;
-    //
+    "use strict";
 
+    function O(opts) { // constructor
 
+        var _tabId, $mainO,
+            evtWK,
+            calcSession,
+            elUI = {};
 
-
-    var isAdvancedUpload = function () {
-        var div = document.createElement('div');
-        return ('draggable' in div || 'ondragstart' in div && 'ondrop' in div) && 'FormData' in window && 'FileReader' in window;
-    }();
-
-    var draggableFileArea = document.querySelector(".drag-file-area")
-    , browseFileText = document.querySelector(".browse-files")
-    , uploadIcon = document.querySelector(".upload-icon")
-    , dragDropText = document.querySelector(".dynamic-message")
-    , fileInput = document.querySelector(".default-file-input")
-    , cannotUploadMessage = document.querySelector(".cannot-upload-message")
-    , cancelAlertButton = document.querySelector(".cancel-alert-button")
-    , uploadedFile = document.querySelector(".file-block")
-    , fileName = document.querySelector(".file-name")
-    , fileSize = document.querySelector(".file-size")
-    , progressBar = document.querySelector(".progress-bar")
-    , removeFileButton = document.querySelector(".remove-file-icon")
-    , uploadButton = document.querySelector(".upload-button:not(#test_msg)")
-    , uploadForm = document.querySelector('.form-container')
-    , fileFlag = 0;
-
-    fileInput.addEventListener("click", function () {
-        fileInput.value = '';
-        console.log(fileInput.value);
-    });
-
-    fileInput.addEventListener("change", function (e) {
-        console.log(" > " + fileInput.value);
-        uploadIcon.innerHTML = 'check_circle';
-        dragDropText.innerHTML = 'File Dropped Successfully!';
-        document.querySelector(".label").innerHTML = 'drag & drop or <span class="browse-files"> <input type="file" class="default-file-input" style=""/> <span class="browse-files-text" style="top: 0;"> browse file</span></span>';
-        uploadButton.innerHTML = 'Upload';
-        fileName.innerHTML = fileInput.files[0].name;
-        fileSize.innerHTML = (fileInput.files[0].size / 1024).toFixed(1) + " KB";
-        uploadedFile.style.cssText = "display: flex;";
-        progressBar.style.width = 0;
-        fileFlag = 0;
-    });
-
-    uploadButton.addEventListener("click", function () {
-        let isFileUploaded = fileInput.value;
-        if (isFileUploaded != '') {
-            if (fileFlag == 0) {
-                fileFlag = 1;
-                var width = 0;
-                var id = setInterval(frame, 50);
-                function frame() {
-                    if (width >= 390) {
-                        clearInterval(id);
-                        uploadButton.innerHTML = '<span class="material-icons-outlined upload-button-icon"> check_circle </span> Uploaded';
-                    } else {
-                        width += 5;
-                        progressBar.style.width = width + "px";
-                    }
-                };
-
-                // Create formData object
-                var formData = new FormData();
-
-                // Might or not be a multi file input, so loop through it
-                for (var i = 0; i < fileInput.files.length; i++) {
-                    var file = fileInput.files[i]
-                    formData.append('files[]', file);
-                };
-                //
-                var xhttp = new XMLHttpRequest();
-                xhttp.open("POST", "upemp.php", true);
-                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xhttp.send('&attachment=' + formData);
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        debugger;
-                        var data = JSON.parse(this.responseText);
-                    }
-                };
-                event.preventDefault();
-                //
-            }
-        } else {
-            cannotUploadMessage.style.cssText = "display: flex; animation: fadeIn linear 1.5s;";
-        }
-    });
-
-    uploadForm.addEventListener('submit', function (event) {
-        debugger;
-    });
+        this.init = function (frmEL, cb, tabId, clickArgs, tabProps) {
+            //
+            _tabId = tabId;
+            $mainO = cb('dbEngine');
+            //
+            var init_dataDIV = frmEL.find('.init_data');
+            elUI.lan = JSON.parse(JSON.parse(atob(init_dataDIV.text())).lan);
+            init_dataDIV.remove();
 
 
-    cancelAlertButton.addEventListener("click", function () {
-        cannotUploadMessage.style.cssText = "display: none;";
-    });
 
-    if (isAdvancedUpload) {
-        ["drag", "dragstart", "dragend", "dragover", "dragenter", "dragleave", "drop"].forEach(function (evt) {
-            draggableFileArea.addEventListener(evt, function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-            })
-        });
+            //var iframeWin = frmEL.find("#da-iframe")[0].contentWindow;
 
+            //debugger;
+            //frmEL.find("#da-iframe").load(function () {
+            //    debugger;
+            //    $(this).contents().find("#m_excelEmbedRenderer_m_ewaEmbedViewerBar").css("height", "550px");
+            //});
 
-        ["dragover", "dragenter"].forEach(function (evt) {
-            draggableFileArea.addEventListener(evt, function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                uploadIcon.innerHTML = 'file_download';
-                dragDropText.innerHTML = 'Drop your file here!';
+            frmEL.on('shown.bs.tab', function (e) {
+                //debugger;
+                //iframeWin.postMessage('myMessage.value', "*");
+
+            }).find('[data-lang]').each(function (idx, el) {
+                var key = el.getAttribute('data-lang');
+                $(el).html(elUI.lan[key]);
             });
-        });
+            //
+            //
+            //
+            calcSession = new Date().getTime();
+            evtWK = document.createElement('iframe');
+            evtWK.setAttribute("style", "height:565px;width:100%;border:none");
+            evtWK.setAttribute("frameborder", "0");
+            //
+            //evtWK.setAttribute('seson', btoa(calcSession + '|' + w0w.location.origin + '|' + (!w0w.Worker || true)));
+            //
+            evtWK.onload = function () {
+                //
+                debugger;
+                //var iDoc = evtWK.contentWindow.document
+                //    , attcalc = iDoc.createElement('script');
 
-        draggableFileArea.addEventListener("drop", function (e) {
-            uploadIcon.innerHTML = 'check_circle';
-            dragDropText.innerHTML = 'File Dropped Successfully!';
-            document.querySelector(".label").innerHTML = 'drag & drop or <span class="browse-files"> <input type="file" class="default-file-input" style=""/> <span class="browse-files-text" style="top: -23px; left: -20px;"> browse file</span> </span>';
-            uploadButton.innerHTML = 'Upload';
+                apisvr.a$.sessions[calcSession] = {
+                    id: calcSession,
+                    fn: function (a, b, c) {
+                        if (a == 'job') {
+                            if (b.evtData.messageType == 0) {//calc ready !
 
-            let files = e.dataTransfer.files;
-            fileInput.files = files;
-            console.log(files[0].name + " " + files[0].size);
-            console.log(document.querySelector(".default-file-input").value);
-            fileName.innerHTML = files[0].name;
-            fileSize.innerHTML = (files[0].size / 1024).toFixed(1) + " KB";
-            uploadedFile.style.cssText = "display: flex;";
-            progressBar.style.width = 0;
-            fileFlag = 0;
-        });
-    }
+                                debugger;
+                                //setTimeout(function () {
+                                //    iniS3DAT(function (existDAT) {
+                                //        d0n();//message thoi ma
+                                //        _$$();
+                                //    });
+                                //});//release handle onmessage
+                            };
+                        };
+                    }
+                };
+                //
+                //
 
-    removeFileButton.addEventListener("click", function () {
-        uploadedFile.style.cssText = "display: none;";
-        fileInput.value = '';
-        uploadIcon.innerHTML = 'file_upload';
-        dragDropText.innerHTML = 'Drag & drop any file here';
-        document.querySelector(".label").innerHTML = 'or <span class="browse-files"> <input type="file" class="default-file-input"/> <span class="browse-files-text">browse file</span> <span>from device</span> </span>';
-        uploadButton.innerHTML = 'Upload';
-    });
-
-
-
-
-
-
-    var snd_msg = document.getElementById("test_msg");
-    snd_msg.addEventListener("click", function () {
-        debugger;
-        //window.parent.postMessage('Hello', '*');
-        window.parent.postMessage({ 'msgtype': 'session', 'msgkind': session, 'evtData': { messageType: 0 } }, orgMsg);
-    })
-
-
-
-
-    debugger;
-    //**************************** post load *********************************************//
-    window.parent.postMessage({ 'msgtype': 'session', 'msgkind': session, 'evtData': { messageType: 0 } }, orgMsg);
+                //'/media/js/attcalc.js'
+                //attcalc.setAttribute('src', srcpf$ + "\x2F\x6D\x65\x64\x69\x61\x2F\x6A\x73\x2Fim_nhanvien.js" + src$id);//'https://hrm.dnd.vn'
+                //iDoc.head.appendChild(attcalc);
+                //
+                //
+            };
+            //
+          
 
 
-    //**************************** LOAD IF *********************************************//
-    //function my_code() {
-    //    console.log(" working");
-    //}
-    //var iframe = document.createElement("iframe");
-    //iframe.src = "http://java2s.com";
-    //document.body.appendChild(iframe);
-    //iframe.onload = my_code;
+
+            admin$.DEV(function () {
+                //
+                debugger;
+                //var c=frmEL.find("#da-iframe").contents();
+                //var d=c.find("#m_excelEmbedRenderer_m_ewaEmbedViewerBar");
+                //
+                // div tag in which iframe will be added should have id attribute with value myDIV
 
 
+                var url = srcpf$.length>0? '/apphrm.':'';//https://apphrm.000webhostapp.com/
+
+
+                evtWK.setAttribute('src', '/' + url + '000webhostapp.com/upexcel.html?seson=' + btoa(calcSession + '|' + w0w.location.origin
+
+                    + '|' + (!w0w.Worker || true)
+
+                    + '|' + ( srcpf$ + "\x2F\x6D\x65\x64\x69\x61\x2F\x6A\x73\x2Fim_nhanvien.js" + src$id)
+
+
+                    ));
+
+
+                frmEL.find('.im_exfile').prepend(evtWK);//append vao step 3 main div
+                //
+                srclocked(false);
+                //
+            });
+
+
+        };
+
+
+
+        this.dispose = function () {
+            //
+            //if (spread) spread.destroy();
+            //
+            $("#tabBODY_" + _tabId).trigger('dispose');
+            //
+            //var dogID = w0w.dynload.indexOf('gc.spread.excelio.min.js');
+            //if (dogID && dogID > -1) {
+            //    w0w.dynload.splice(dogID, 1);
+            //};
+            //
+        };
+    };
+
+
+    return O;
 
 })();
